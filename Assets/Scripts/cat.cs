d047384b1ed7e4cd3ba140fcc3be537f2b1fd591
yolanda octaviane <yolandaoctavi@gmail.com>
2022-02-07 21:15:18 +0700

    cat script

---
+++ Assets/Scripts/cat.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cat : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] Transform[] positions;
+    [SerializeField] float speed;
+
+    int posIndex;
+    Transform nextPos;
+
+    private void Start()
+    {
+        nextPos = positions[0];
+    }
+
+    private void Update()
+    {
+        catMove();
+    }
+
+    void catMove()
+    {
+        if (transform.position == nextPos.position)
+        {
+            posIndex++;
+            if (posIndex >= positions.Length) return;
+            nextPos = positions[posIndex];
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPos.position, speed * Time.deltaTime);
+        }
+    }
+}
