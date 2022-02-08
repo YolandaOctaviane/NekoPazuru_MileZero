c251c73a8002115ffd4279d67f3858dc7a456f53
yolanda octaviane <yolandaoctavi@gmail.com>
2022-02-08 13:36:06 +0700

    bridge

--- Assets/Scripts/Bridge.cs
+++ Assets/Scripts/Bridge.cs
@@ -1,18 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Bridge : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float timer = 2;
+    [SerializeField] bool timeIsRunning = false;
+    private float defaultTimer;
+    private Cat cat;
+
+    private void Start()
     {
-        
+        defaultTimer = timer;
+
+        if (GetComponent<SpriteRenderer>().enabled)
+        {
+            timeIsRunning = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        cat = collision.GetComponent<Cat>();
+        if (cat != null)
+        {
+            timeIsRunning = false;
+            //death if no bridge
+            if (GetComponent<SpriteRenderer>().enabled == false)
+            {
+                cat.isDed = true;
+            }
+        }
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        cat = collision.GetComponent<Cat>();
+        if (cat != null)
+        {
+            timeIsRunning = false;
+            timer = defaultTimer;
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (timeIsRunning)
+        {
+            if (timer > 0f)
+            {
+                timer -= Time.deltaTime;
+            }
+            else
+            {
+                timeIsRunning = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+            }
+        }
     }
 }
